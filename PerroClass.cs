8764f62a55ebace413b9166b2a625749c485f50d
Ivan Firestone <ivan.firestone@icloud.com>
2023-07-15 10:09:48 -0400

    Orientacion a Objetos

---
+++ PerroClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace animales
+{
+	public class PerroClass
+	{
+		public PerroClass(string nombre, string edad, string color)
+		{
+            this.edad = edad;
+            this.nombre = nombre;
+            this.color = color;
+		}
+
+        private string? nombre;
+        private string? edad;
+        private string? color;
+        private string? raza;
+
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public string getEdad()
+        {
+            return this.edad;
+        }
+
+        public string getColor()
+        {
+            return this.color;
+        }
+
+        public string getRaza()
+        {
+            return this.raza;
+        }
+
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void setEdad(string edad)
+        {
+            this.edad = edad;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+        public void setRaza(string raza)
+        {
+            this.raza = raza;
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Clases Programacion 1");
+
+animales.PerroClass perro = new("Lucrecia", "2 años", "Marron Verdoso");
+
+
+//perro.setNombre("Lucrecia");
+perro.setEdad("10 años");
+//perro.setColor("Marron Oscuro Verdoso");
+perro.setRaza("Gran Danés");
+
+
+Console.WriteLine("Nombre: " + perro.getNombre());
+Console.WriteLine("Edad: " + perro.getEdad());
+Console.WriteLine("Color: " + perro.getColor());
+Console.WriteLine("Raza: " + perro.getRaza());
