fd683f9696d5326c9b989dcfb4a7e1fd1000a279
Ivan Firestone <ivan.firestone@icloud.com>
2023-07-22 10:34:47 -0700

    Mejoras
    Mejoras

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+transportacion.Carro carro = new("2.4 Litros", 4, 5, 6, true, "gasolina", "rojo", "Honda", 200000);
 
-Console.WriteLine("Clases Programacion 1");
+transportacion.Camion camion = new("7.8 Litros", 16, 2, 2, false, "diesel", "naranja", "MACK", 8000000, 3500, 3);
 
-animales.PerroClass perro = new("Lucrecia", "2 años", "Marron Verdoso");
+Console.WriteLine("CARRO:");
+Console.WriteLine(carro.ToString());
+Console.WriteLine("CAMION:");
+Console.WriteLine(camion.ToString());
 
+animales.Gato gato = new("Mamifero", 4, 2, "Gris", "Mauricio", "Angora");
 
-//perro.setNombre("Lucrecia");
-perro.setEdad("10 años");
-//perro.setColor("Marron Oscuro Verdoso");
-perro.setRaza("Gran Danés");
+animales.Perro perro = new("Mamifero", 4, 5, "Marron", "Buster", "PitBull");
 
-
-Console.WriteLine("Nombre: " + perro.getNombre());
-Console.WriteLine("Edad: " + perro.getEdad());
-Console.WriteLine("Color: " + perro.getColor());
-Console.WriteLine("Raza: " + perro.getRaza());
+Console.WriteLine("PERRO:");
+Console.WriteLine(perro.ToString());
+Console.WriteLine("GATO:");
+Console.WriteLine(gato.ToString());
